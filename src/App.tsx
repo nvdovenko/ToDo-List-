c919e424a93f98705523083dbc36e71ebc729f17
Anastasiia Vdovenko <anastasiia.vdovenko@learnit.fi>
2024-10-27 14:39:25 +0200

    Added work with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import todoListImg from "./assets/todo-icon.jpg";
 import Header from "./components/Header.tsx";
 import NewTask from "./components/NewTask.tsx";
@@ -12,19 +12,37 @@ export type ToDoTask = {
 export default function App() {
   const [tasks, setTasks] = useState<ToDoTask[]>([]); // array of tasks
 
+  // Load tasks from localStorage
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    setTasks(storedTasks);
+  }, []);
+
   function handleAddTask(task: string, summary: string) {
-    setTasks((prevTasks) => {
-      const newTask: ToDoTask = {
-        id: Math.random(),
-        title: task,
-        description: summary,
-      };
-      return [...prevTasks, newTask];
-    });
+    const newTask: ToDoTask = {
+      id: Math.random(),
+      title: task,
+      description: summary,
+    };
+
+    // Update tasks in localStorage
+    const updatedTasks = [...tasks, newTask];
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+
+    // Update state with the new task
+    setTasks(updatedTasks);
   }
 
   function handleDeleteTask(id: number) {
+    deleteTaskFromLocalStorage(id);
     setTasks((prevTask) => prevTask.filter((task) => task.id !== id));
+  }
+
+  // Delete task from localStorage
+  function deleteTaskFromLocalStorage(id: number) {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    const updatedTasks = storedTasks.filter((task: ToDoTask) => task.id !== id);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
   }
 
   return (
